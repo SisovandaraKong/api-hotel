93198b5d626a21a162be7bdbfdf3e96185685544
Kong_Sisovandara <sisovandara@gmail.com>
2025-06-05 09:06:01 +0700

    uplaod s3 last

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -10,20 +10,30 @@ use Illuminate\Support\Facades\Storage;
 
 class ImageUploadController extends Controller
 {
+    // public function uploadImage(Request $request)
+    // {
+    //     $path = $request->file('image')->storePublicly('public/images');
+    //     return response()->json([
+    //         'path'=> "https://romsaydev.s3.us-east-1.amazonaws.com/$path",     
+    //         'msg' => 'Image uploaded successfully',
+    //     ]);
+    // }  
     public function uploadImage(Request $request)
-    {
-        $path = $request->file('image')->storePublicly('public/images');
-        return response()->json([
-            'path'=> "https://romsaydev.s3.us-east-1.amazonaws.com/$path",     
-            'msg' => 'Image uploaded successfully',
-        ]);
-    }  
-    
-    // get all images
-    public function getAllImages()
-    {
-        $files = Storage::files('public/images');
-        $images = array_map(function ($file) {
+{
+    $path = $request->file('image')->store('images', 's3');
+
+    return response()->json([
+        'path' => "https://romsaydev.s3.us-east-1.amazonaws.com/$path",
+        'success' => true,
+        'msg' => 'Image uploaded successfully',
+    ]);
+}
+
+// get all images
+public function getAllImages()
+{
+    $files = Storage::files('images');
+    $images = array_map(function ($file) {
             return "https://romsaydev.s3.us-east-1.amazonaws.com/$file";
         }, $files);
 
